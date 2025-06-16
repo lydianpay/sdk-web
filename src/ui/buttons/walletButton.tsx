98d69767627215da490721bf8f3f63a0ff78d7ee
Derek Sanford <derek@tetherpaymentsgroup.com>
2025-06-16 09:34:35 -0400

    feat(manual qr): adding manual qr as a wallet button

--- src/ui/buttons/walletButton.tsx
+++ src/ui/buttons/walletButton.tsx
@@ -2,7 +2,7 @@ import { h } from "tsx-dom";
 import { WalletConnectWallet } from '../../types';
 
 export default function WalletButton(wallet: WalletConnectWallet) {
-    const walletBtnId = `btnWallet${wallet.name}`
+    const walletBtnId = `btnWallet${wallet.id}`
     return (
         <button id={walletBtnId}
             class="w-full flex items-center space-x-2 border border-gray-300 rounded-lg px-4 py-2 hover:bg-gray-50">
