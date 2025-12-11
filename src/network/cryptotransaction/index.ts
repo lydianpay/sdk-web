6420e30b820f19ef27a6f27904af5ee94b0eba6e
Sulav Neupane <sulav@Sulavs-MacBook-Pro-2.local>
2025-12-11 16:35:23 -0500

    chore(kyc-travel-rules): added KYC info form if kyc or travel rules flow is triggered

--- src/network/cryptotransaction/index.ts
+++ src/network/cryptotransaction/index.ts
@@ -5,6 +5,7 @@ import {
   CreateTransactionRequest,
   CreateTransactionResponse,
   GetTransactionResponse,
+  KYCVerificationRequest,
 } from '../../types';
 
 const resourceName = 'transaction';
@@ -32,6 +33,29 @@ export class CryptoTransactions extends Base {
     return await this.request<CreateTransactionResponse>(`/${resourceName}/${transactionID}/cancel`, {
       method: 'PATCH',
       body: JSON.stringify(cancelTransaction),
+    });
+  }
+
+  async kycVerification(transactionID: string, kycVerificationRequest: KYCVerificationRequest) {
+    const formData = new FormData();
+    formData.append('asset', kycVerificationRequest.asset);
+    formData.append('network', kycVerificationRequest.network);
+    formData.append('firstName', kycVerificationRequest.firstName);
+    formData.append('lastName', kycVerificationRequest.lastName);
+    formData.append('email', kycVerificationRequest.email);
+    formData.append('street', kycVerificationRequest.street);
+    formData.append('city', kycVerificationRequest.city);
+    formData.append('region', kycVerificationRequest.region);
+    formData.append('postalCode', kycVerificationRequest.postalCode);
+    formData.append('country', kycVerificationRequest.country);
+    formData.append('documentType', kycVerificationRequest.documentType);
+    for (const file of kycVerificationRequest.documentFiles) {
+      formData.append('documentFiles', file);
+    }
+
+    return await this.requestWithFormData<CreateTransactionResponse>(`/${resourceName}/${transactionID}/verification`, {
+      method: 'POST',
+      body: formData,
     });
   }
 }
